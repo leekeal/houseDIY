5622164b30fcd46b5625878570e2a1e431d80483
fwind <risyo520@gmail.com>
2014-01-01 06:00:57 +0900

    diy-main自适应高度

--- app/scripts/views/diy_view.js
+++ app/scripts/views/diy_view.js
@@ -1,7 +1,27 @@
 App.DiyView = Ember.View.extend({
-	templateName: 'diy',
-	pageName: 'div-main',
-		click:function(){
-			console.log("div-view");
-		}
+	diyMainHeight:null,
+	diyMainstyle:null,
+
+	didInsertElement:function(){
+		self = this;
+		self.setDiyMainHeight(self);
+		$(window).bind('resize',function(){
+			self.setDiyMainHeight(self);
+		});
+	},
+
+
+	setDiyMainHeight:function(DiyView){
+		var windowHeight = $(window).height();
+		var headerHeight = $("header").height();
+		var diyTopHeight = $(".diy-top").height();
+		var footerHeight = $("footer").height();
+		DiyView.set('diyMainHeight',windowHeight - headerHeight - diyTopHeight - footerHeight);
+		DiyView.set('diyMainstyle','height:'+DiyView.get('diyMainHeight')+'px;');
+	},
+
+
+
+
+
 });
